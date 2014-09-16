1d30566fe876a898900a9d532355cda1a8e9a2ee
Jason Hancock <jhancock@llnw.com>
2014-09-16 16:12:33 -0700

    Configure dev env to use apache instead of "make serve"

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -5,3 +5,11 @@ apt-get -y update
 export DEBIAN_FRONTEND=noninteractive
 
 cat /vagrant/vagrant/packages | xargs apt-get install -y
+
+
+ln -s /vagrant/vagrant/apache.conf /etc/apache2/sites-available/betatestbrewing.conf
+
+a2ensite betatestbrewing
+a2dissite 000-default
+
+service apache2 restart
